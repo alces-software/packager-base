4cd7392ad751f27615e3974fdaa10f1988b7ca9c
Ruan G. Ellis <ruan.ellis@alces-software.com>
2017-12-15 18:03:13 +0000

    Created Gridware; GATK v4.beta.6

---
+++ apps/gatk/4.beta.6/gatk-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+n=$(basename $0)
+if [ "$n" == 'gatk' -o "$n" == 'GenomeAnalysisTK' ]; then
+  n=""
+elif [[ "$n" == gatk_* ]]; then
+  n=$(echo "$n" | cut -c6-)
+  t="-T"
+else
+  echo "$0: unable to determine GATK task to execute."
+  exit 1
+fi
+exec java `eval echo $GATK_JAVA_OPTS` -jar "$GATKDIR/gatk-package-4.beta.6-local.jar" $t $n "$@"
